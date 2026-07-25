3189200a0cf8d4fabf3469db5f6e2a7b37ac71fc
weemakes <weemakes01@gmail.com>
2026-07-25 17:34:51 +0530

    initDB method is commented from server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Initialize PostgreSQL Database tables
-initDb();
+// initDb();
 
 // Routes
 app.use('/api/vendor', vendorRoutes);
