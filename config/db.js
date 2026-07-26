1d35876292de6de46172a8c8ef654c077bfd715c
MarufMehar <marufkhan1001gg@gmail.com>
2026-07-26 17:27:01 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -5,20 +5,23 @@ dotenv.config();
 
 const { Pool } = pkg;
 
+const sslOption = process.env.PG_SSL === 'true' ? { rejectUnauthorized: false } : false;
+
 const pool = new Pool({
   host: process.env.PG_HOST || 'localhost',
   port: process.env.PG_PORT || 5432,
   user: process.env.PG_USER || 'postgres',
   password: process.env.PG_PASSWORD || 'inframe123',
   database: process.env.PG_DATABASE || 'ecom_db',
-    ssl: {
-    rejectUnauthorized: false,
-  },
-}
-);
+  ...(sslOption ? { ssl: sslOption } : {}),
+});
 
+let isConnectedLogged = false;
 pool.on('connect', () => {
-  console.log('Connected to PostgreSQL database');
+  if (!isConnectedLogged) {
+    console.log('Connected to PostgreSQL database');
+    isConnectedLogged = true;
+  }
 });
 
 pool.on('error', (err) => {
